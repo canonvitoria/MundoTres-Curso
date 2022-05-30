8e04dbd3b23870eefb183f972724262d74e26fd4
Vitória Canon <98993613+Miyamizuu@users.noreply.github.com>
2022-05-30 19:40:42 -0300

    Add files via upload

---
+++ Aula - 022.py
@@ -0,0 +1,7 @@
+from Úteis import Numeros
+
+num = int(input("Digite um número: "))
+fat = Numeros.fatorial(num)
+print(f"O fatorial de {num} é {fat}")
+print(f'O dobro de {num} é {Numeros.dobro(num)}')
+print(f"O triplo de {num} é {Numeros.triplo(num)}")
